26090ea0270bc351daf2980adc959a1e95b5045d
boogeymanCoder <72642782+boogeymanCoder@users.noreply.github.com>
2022-01-30 13:54:01 +0800

    Revert "Issue/13"

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,51 +1,60 @@
-import Head from "next/head";
-import { Box, Container, Grid, Typography } from "@mui/material";
-import { AccountProfile } from "../components/account/account-profile";
-import { AccountProfileDetails } from "../components/account/account-profile-details";
-import { DashboardLayout } from "../components/dashboard-layout";
-import { getAuth } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Head from 'next/head';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { AccountProfile } from '../components/account/account-profile';
+import { AccountProfileDetails } from '../components/account/account-profile-details';
+import { DashboardLayout } from '../components/dashboard-layout';
 
-const Account = () => {
-  const router = useRouter();
-  const auth = getAuth();
-  const [user, userLoading, userError] = useAuthState(auth);
-
-  useEffect(() => {
-    if (!user && !userLoading) router.push("/login");
-  }, [user, userLoading]);
-  return (
-    <>
-      <Head>
-        <title>Account | Material Kit</title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8,
-        }}
-      >
-        <Container maxWidth="lg">
-          <Typography sx={{ mb: 3 }} variant="h4">
-            Account
-          </Typography>
-          <Grid container spacing={3}>
-            <Grid item lg={4} md={6} xs={12}>
-              <AccountProfile />
-            </Grid>
-            <Grid item lg={8} md={6} xs={12}>
-              <AccountProfileDetails />
-            </Grid>
+const Account = () => (
+  <>
+    <Head>
+      <title>
+        Account | Material Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography
+          sx={{ mb: 3 }}
+          variant="h4"
+        >
+          Account
+        </Typography>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xs={12}
+          >
+            <AccountProfile />
           </Grid>
-        </Container>
-      </Box>
-    </>
-  );
-};
+          <Grid
+            item
+            lg={8}
+            md={6}
+            xs={12}
+          >
+            <AccountProfileDetails />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
 
-Account.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+Account.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 
 export default Account;
--- src/pages/customers.js
+++ src/pages/customers.js
@@ -1,44 +1,37 @@
-import Head from "next/head";
-import { Box, Container } from "@mui/material";
-import { CustomerListResults } from "../components/customer/customer-list-results";
-import { CustomerListToolbar } from "../components/customer/customer-list-toolbar";
-import { DashboardLayout } from "../components/dashboard-layout";
-import { customers } from "../__mocks__/customers";
-import { getAuth } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import router, { useRouter } from "next/router";
-import { useEffect } from "react";
+import Head from 'next/head';
+import { Box, Container } from '@mui/material';
+import { CustomerListResults } from '../components/customer/customer-list-results';
+import { CustomerListToolbar } from '../components/customer/customer-list-toolbar';
+import { DashboardLayout } from '../components/dashboard-layout';
+import { customers } from '../__mocks__/customers';
 
-function Customers() {
-  const router = useRouter();
-  const auth = getAuth();
-  const [user, userLoading, userError] = useAuthState(auth);
-
-  useEffect(() => {
-    if (!user && !userLoading) router.push("/login");
-  }, [user, userLoading]);
-  return (
-    <>
-      <Head>
-        <title>Customers | Material Kit</title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8,
-        }}
-      >
-        <Container maxWidth={false}>
-          <CustomerListToolbar />
-          <Box sx={{ mt: 3 }}>
-            <CustomerListResults customers={customers} />
-          </Box>
-        </Container>
-      </Box>
-    </>
-  );
-}
-Customers.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+const Customers = () => (
+  <>
+    <Head>
+      <title>
+        Customers | Material Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <CustomerListToolbar />
+        <Box sx={{ mt: 3 }}>
+          <CustomerListResults customers={customers} />
+        </Box>
+      </Container>
+    </Box>
+  </>
+);
+Customers.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 
 export default Customers;
