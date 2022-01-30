38a087eef63c9637d4e9a49921fa6f33be530a32
boogeymanCoder <72642782+boogeymanCoder@users.noreply.github.com>
2022-01-30 15:26:54 +0800

    Revert "Issue/12"

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,50 +1,66 @@
-import Head from "next/head";
-import { Box, Container, Grid, Pagination } from "@mui/material";
-import { products } from "../__mocks__/products";
-import { ProductListToolbar } from "../components/product/product-list-toolbar";
-import { ProductCard } from "../components/product/product-card";
-import { DashboardLayout } from "../components/dashboard-layout";
-import CheckAuth from "src/components/auth/CheckAuth";
+import Head from 'next/head';
+import { Box, Container, Grid, Pagination } from '@mui/material';
+import { products } from '../__mocks__/products';
+import { ProductListToolbar } from '../components/product/product-list-toolbar';
+import { ProductCard } from '../components/product/product-card';
+import { DashboardLayout } from '../components/dashboard-layout';
 
-const Products = () => {
-  return (
-    <CheckAuth>
-      <Head>
-        <title>Products | Material Kit</title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8,
-        }}
-      >
-        <Container maxWidth={false}>
-          <ProductListToolbar />
-          <Box sx={{ pt: 3 }}>
-            <Grid container spacing={3}>
-              {products.map((product) => (
-                <Grid item key={product.id} lg={4} md={6} xs={12}>
-                  <ProductCard product={product} />
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              pt: 3,
-            }}
+const Products = () => (
+  <>
+    <Head>
+      <title>
+        Products | Material Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <ProductListToolbar />
+        <Box sx={{ pt: 3 }}>
+          <Grid
+            container
+            spacing={3}
           >
-            <Pagination color="primary" count={3} size="small" />
-          </Box>
-        </Container>
-      </Box>
-    </CheckAuth>
-  );
-};
+            {products.map((product) => (
+              <Grid
+                item
+                key={product.id}
+                lg={4}
+                md={6}
+                xs={12}
+              >
+                <ProductCard product={product} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            pt: 3
+          }}
+        >
+          <Pagination
+            color="primary"
+            count={3}
+            size="small"
+          />
+        </Box>
+      </Container>
+    </Box>
+  </>
+);
 
-Products.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+Products.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 
 export default Products;
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,93 +1,133 @@
-import Head from "next/head";
-import NextLink from "next/link";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import {
-  Alert,
   Box,
   Button,
   Checkbox,
   Container,
   FormHelperText,
-  IconButton,
-  InputAdornment,
   Link,
-  Snackbar,
   TextField,
-  Typography,
-} from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useEffect, useState } from "react";
-import { getAuth } from "firebase/auth";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
-import CheckNonAuth from "src/components/auth/CheckNonAuth";
+  Typography
+} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const Register = () => {
-  const [showError, setShowError] = useState(false);
-
-  const auth = getAuth();
-  const [createUserWithEmailAndPassword, emailUser, emailLoading, emailError] =
-    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
-
-  useEffect(() => {
-    if (emailError) setShowError(true);
-  }, [emailError]);
-
-  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
   const formik = useFormik({
     initialValues: {
-      email: "",
-      password: "",
-      policy: false,
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      policy: false
     },
     validationSchema: Yup.object({
-      email: Yup.string().email("Must be a valid email").max(255).required("Email is required"),
-      password: Yup.string()
+      email: Yup
+        .string()
+        .email(
+          'Must be a valid email')
         .max(255)
-        .required("Password is required")
-        .matches(/.{8,}/, "Password must have at eight characters")
-        .matches(/[0-9]+/, "Password must have at least one [0-9] numerical character")
-        .matches(/[a-z]+/, "Password must have at least one [a-z] lower case alphabet")
-        .matches(/[A-Z]+/, "Password must have at least one [A-Z] upper case alphabet")
-        .matches(/[^a-zA-Z0-9]+/, "Password must have at least one [!$*...] special character"),
-      policy: Yup.boolean().oneOf([true], "This field must be checked"),
+        .required(
+          'Email is required'),
+      firstName: Yup
+        .string()
+        .max(255)
+        .required(
+          'First name is required'),
+      lastName: Yup
+        .string()
+        .max(255)
+        .required(
+          'Last name is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required(
+          'Password is required'),
+      policy: Yup
+        .boolean()
+        .oneOf(
+          [true],
+          'This field must be checked'
+        )
     }),
-    onSubmit: (values) => {
-      return createUserWithEmailAndPassword(values.email, values.password);
-    },
+    onSubmit: () => {
+      router.push('/');
+    }
   });
 
   return (
-    <CheckNonAuth>
+    <>
       <Head>
-        <title>Register | Material Kit</title>
+        <title>
+          Register | Material Kit
+        </title>
       </Head>
       <Box
         component="main"
         sx={{
-          alignItems: "center",
-          display: "flex",
+          alignItems: 'center',
+          display: 'flex',
           flexGrow: 1,
-          minHeight: "100%",
+          minHeight: '100%'
         }}
       >
         <Container maxWidth="sm">
-          <NextLink href="/" passHref>
-            <Button component="a" startIcon={<ArrowBackIcon fontSize="small" />}>
+          <NextLink
+            href="/"
+            passHref
+          >
+            <Button
+              component="a"
+              startIcon={<ArrowBackIcon fontSize="small" />}
+            >
               Dashboard
             </Button>
           </NextLink>
-          <form onSubmit={formik.handleSubmit} noValidate>
+          <form onSubmit={formik.handleSubmit}>
             <Box sx={{ my: 3 }}>
-              <Typography color="textPrimary" variant="h4">
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
                 Create a new account
               </Typography>
-              <Typography color="textSecondary" gutterBottom variant="body2">
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
                 Use your email to create a new account
               </Typography>
             </Box>
-
+            <TextField
+              error={Boolean(formik.touched.firstName && formik.errors.firstName)}
+              fullWidth
+              helperText={formik.touched.firstName && formik.errors.firstName}
+              label="First Name"
+              margin="normal"
+              name="firstName"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.firstName}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.lastName && formik.errors.lastName)}
+              fullWidth
+              helperText={formik.touched.lastName && formik.errors.lastName}
+              label="Last Name"
+              margin="normal"
+              name="lastName"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.lastName}
+              variant="outlined"
+            />
             <TextField
               error={Boolean(formik.touched.email && formik.errors.email)}
               fullWidth
@@ -100,7 +140,6 @@ const Register = () => {
               type="email"
               value={formik.values.email}
               variant="outlined"
-              required
             />
             <TextField
               error={Boolean(formik.touched.password && formik.errors.password)}
@@ -111,26 +150,15 @@ const Register = () => {
               name="password"
               onBlur={formik.handleBlur}
               onChange={formik.handleChange}
-              type={showPassword ? "text" : "password"}
+              type="password"
               value={formik.values.password}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={(e) => setShowPassword(!showPassword)} edge="end">
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
               variant="outlined"
-              required
             />
-
             <Box
               sx={{
-                alignItems: "center",
-                display: "flex",
-                ml: -1,
+                alignItems: 'center',
+                display: 'flex',
+                ml: -1
               }}
             >
               <Checkbox
@@ -138,17 +166,30 @@ const Register = () => {
                 name="policy"
                 onChange={formik.handleChange}
               />
-              <Typography color="textSecondary" variant="body2">
-                I have read the{" "}
-                <NextLink href="#" passHref>
-                  <Link color="primary" underline="always" variant="subtitle2">
+              <Typography
+                color="textSecondary"
+                variant="body2"
+              >
+                I have read the
+                {' '}
+                <NextLink
+                  href="#"
+                  passHref
+                >
+                  <Link
+                    color="primary"
+                    underline="always"
+                    variant="subtitle2"
+                  >
                     Terms and Conditions
                   </Link>
                 </NextLink>
               </Typography>
             </Box>
             {Boolean(formik.touched.policy && formik.errors.policy) && (
-              <FormHelperText error>{formik.errors.policy}</FormHelperText>
+              <FormHelperText error>
+                {formik.errors.policy}
+              </FormHelperText>
             )}
             <Box sx={{ py: 2 }}>
               <Button
@@ -162,10 +203,20 @@ const Register = () => {
                 Sign Up Now
               </Button>
             </Box>
-            <Typography color="textSecondary" variant="body2">
-              Have an account?{" "}
-              <NextLink href="/login" passHref>
-                <Link variant="subtitle2" underline="hover">
+            <Typography
+              color="textSecondary"
+              variant="body2"
+            >
+              Have an account?
+              {' '}
+              <NextLink
+                href="/login"
+                passHref
+              >
+                <Link
+                  variant="subtitle2"
+                  underline="hover"
+                >
                   Sign In
                 </Link>
               </NextLink>
@@ -173,25 +224,7 @@ const Register = () => {
           </form>
         </Container>
       </Box>
-      {emailUser && (
-        <Snackbar open={true}>
-          <Alert severity="success" sx={{ width: "100%" }}>
-            <Typography variant="p" component="p" color="inherit">
-              Account successfully created! - <Link href="/login">Sign in</Link>
-            </Typography>
-          </Alert>
-        </Snackbar>
-      )}
-      {emailError && (
-        <Snackbar open={showError}>
-          <Alert severity="error" sx={{ width: "100%" }} onClose={(e) => setShowError(!showError)}>
-            <Typography variant="p" component="p" color="inherit">
-              {emailError.message}
-            </Typography>
-          </Alert>
-        </Snackbar>
-      )}
-    </CheckNonAuth>
+    </>
   );
 };
 
