bb83f6cf6cbebb1b38cb36d79f536406f4a0da2c
John Bryan Pit M. Acaso <johnbryanpitcasoy@gmail.com>
2022-03-05 16:10:20 +0800

    configured storybook to work for mui

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,25 @@
+const path = require("path");
+const toPath = (filePath) => path.join(process.cwd(), filePath);
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    };
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+};
