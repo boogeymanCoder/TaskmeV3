f54854ea7538f5f89d9157a643af45598be9b006
John Acaso <johnbryanpitcasoy@gmail.com>
2022-03-18 14:15:59 +0800

    Added conversation component

---
+++ src/components/message/Conversation.js
@@ -0,0 +1,105 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Avatar, Badge, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+/**
+ * Displays a conversation.
+ */
+export default function Conversation({
+  avatar,
+  title,
+  lastMessage,
+  lastMessageSent,
+  isRead,
+  onClick,
+}) {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ cursor: "pointer" }}
+      onClick={onClick}
+    >
+      <Grid item sx={{ pr: 1 }}>
+        <Avatar
+          alt={title}
+          src={avatar}
+          sx={{
+            height: 60,
+            width: 60,
+          }}
+        />
+      </Grid>
+      <Grid item xs>
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+          <Grid item xs={12}>
+            <Badge color="primary" variant="dot" invisible={isRead} overlap="rectangular">
+              <Typography variant="h6">{title}</Typography>
+            </Badge>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container sx={{ p: 0 }}>
+              <Grid item xs={9} sx={{ p: 0 }}>
+                <Typography
+                  variant="caption"
+                  color={!isRead ? "primary" : "text"}
+                  noWrap
+                  paragraph
+                  sx={{ m: 0, p: 0 }}
+                >
+                  {lastMessage}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography
+                  variant="caption"
+                  color={!isRead ? "primary" : "text"}
+                  noWrap
+                  paragraph
+                  textAlign="end"
+                  sx={{ m: 0, p: 0 }}
+                >
+                  {lastMessageSent}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+Conversation.propTypes = {
+  /**
+   * The image to show, displays titles, first letter if not supplied.
+   */
+  avatar: PropTypes.string,
+  /**
+   * Title to display to represent the conversation.
+   */
+  title: PropTypes.string.isRequired,
+  /**
+   * Text to show when the last message was sent.
+   */
+  lastMessageSent: PropTypes.string.isRequired,
+  /**
+   * Text to show the last message sent.
+   */
+  lastMessage: PropTypes.string.isRequired,
+  /**
+   * Text to show the last message sender.
+   */
+  lastSender: PropTypes.string.isRequired,
+  /**
+   * Whether to mark the conversation as read or not.
+   */
+  isRead: PropTypes.bool.isRequired,
+  /**
+   * Function to call when clicking the conversation.
+   */
+  onClick: PropTypes.func.isRequired,
+};
---
+++ src/stories/taskme/message/Conversation.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { default as ConversationComponent } from "/src/components/message/Conversation";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const sb = {
+  title: "Message/Conversation",
+  component: ConversationComponent,
+  argTypes: {
+    onClick: { action: "onClick" },
+  },
+};
+export default sb;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <ConversationComponent {...args} />;
+
+export const Read = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Read.args = {
+  avatar:
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  title: "Marry Jane",
+  lastSender: "Mary Jane",
+  lastMessage: "Hello, I would like to follow up the task.",
+  lastMessageSent: "2m ago",
+  isRead: true,
+};
+
+export const NotRead = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+NotRead.args = {
+  avatar:
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  title: "Marry Jane",
+  lastSender: "Mary Jane",
+  lastMessage: "Hello, I would like to follow up the task.",
+  lastMessageSent: "2m ago",
+  isRead: false,
+};
