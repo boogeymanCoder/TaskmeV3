b6f6636b118bd84e93179acb47893305f9c41ac9
John Acaso <johnbryanpitcasoy@gmail.com>
2022-04-20 15:01:24 +0800

    added changed to storybook for edit and delete menu changes on post card

--- src/stories/taskme/post/Post.stories.js
+++ src/stories/taskme/post/Post.stories.js
@@ -9,6 +9,7 @@ const sb = {
     onEdit: { action: "onEdit" },
     onLike: { action: "onLike" },
     onComment: { action: "onComment" },
+    onDelete: { action: "onDelete" },
   },
 };
 export default sb;
@@ -16,13 +17,26 @@ export default sb;
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <PostComponent {...args} />;
 
-export const Post = Template.bind({});
+export const NotOwned = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Post.args = {
+NotOwned.args = {
   avatar:
     "https://images.unsplash.com/photo-1644982647844-5ee1bdc5b114?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
   name: "Melissa Skye",
   lastUpdate: "a day ago",
   details:
     "Hello, I am a student from San Francisco. I am currently having a hard time with math and I was wondering can anyone here help me with, algebra?",
+  isOwned: false,
+};
+
+export const Owned = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Owned.args = {
+  avatar:
+    "https://images.unsplash.com/photo-1644982647844-5ee1bdc5b114?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  name: "Melissa Skye",
+  lastUpdate: "a day ago",
+  details:
+    "Hello, I am a student from San Francisco. I am currently having a hard time with math and I was wondering can anyone here help me with, algebra?",
+  isOwned: true,
 };
